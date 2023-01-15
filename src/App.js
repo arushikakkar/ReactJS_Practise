fbbafa9044fc6ff4e57b472b9ba3563138198055
arushikakkar <aarushikakkarr31@gmail.com>
2023-01-15 18:09:54 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 function App() {
-  return (
+    return ( 
     <>
-    <Navbar title = "TextUtils" aboutText = "AboutUtils"/>   
-       </>
-     
-
-  );
+        <Navbar title="TextUtils" aboutText="AboutUtils"/>
+        <div className="container my-3">
+            <TextForm heading="Enter text to analyze below"/>
+            </div> 
+        </>
+            );
 }
 
-export default App;
+            export default App;
